385afd975404cae915e2471ecc4a2d122f3c592b
GlebPashko <glebpashko2004@gmail.com>
2025-02-03 12:56:51 +0200

    initial project commit

---
+++ src/main/java/com/sportsevents/backend/sportseventsbackend/SportsEventsBackendApplication.java
@@ -0,0 +1,13 @@
+package com.sportsevents.backend.sportseventsbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SportsEventsBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SportsEventsBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sportsevents/backend/sportseventsbackend/SportsEventsBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sportsevents.backend.sportseventsbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SportsEventsBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
